a8b5eb552810ee675a36d324861b773c0c893d29
kenmhaggerty <kenmhaggerty@gmail.com>
2016-01-04 13:16:36 -0500

    Completes tasks for "E. Operation Precedence"

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -13,41 +13,17 @@
      
      */
     
-    BOOL threeIsEqualToThree = 3 == 3;
-    NSLog(@"3 == 3: %d", threeIsEqualToThree);
+    NSInteger x = 2 + 3 * 5;
+    NSLog(@"x: %li", x);
     
-    BOOL fourIsEqualToThree = 4 == 3;
-    NSLog(@"4 == 3: %d", fourIsEqualToThree);
+    x = (2 + 3) * 5;
+    NSLog(@"x: %li", x);
     
-    BOOL fiveIsNotEqualToThree = 5 != 3;
-    NSLog(@"5 != 3: %d", fiveIsNotEqualToThree);
+    NSInteger y = 5 - 8 * 4 + 2;
+    NSLog(@"y: %li", y);
     
-    BOOL sixIsNotEqualToSix = 6 != 6;
-    NSLog(@"6 != 6: %d", sixIsNotEqualToSix);
-    
-    NSLog(@"3 == 3: %d", 3 == 3);
-    NSLog(@"4 == 3: %d", 4 == 3);
-    NSLog(@"5 != 3: %d", 5 != 3);
-    NSLog(@"6 != 6: %d", 6 != 6);
-    
-    NSLog(@"7 < 8: %d", 7 < 8);
-    NSLog(@"8 < 5: %d", 8 < 5);
-    NSLog(@"9 < 9: %d", 9 < 9);
-    NSLog(@"10 <= 10: %d", 10 <= 10);
-    
-    NSLog(@"11 > 8: %d", 11 > 8);
-    NSLog(@"12 > 15: %d", 12 > 15);
-    NSLog(@"13 > 13: %d", 13 > 13);
-    NSLog(@"14 >= 14: %d", 14 >= 14);
-    
-    NSInteger a = 1; // added in because unclear
-    NSInteger b = 0; // added in because unclear
-    BOOL aIsEqualToB = a == b;
-    BOOL aIsGreaterThanB = a > b;
-    BOOL aIsLessThanB = a < b;
-    NSLog(@"a == b: %d", aIsEqualToB);
-    NSLog(@"a > b: %d", aIsGreaterThanB);
-    NSLog(@"a < b: %d", aIsLessThanB);
+    y = 5 - 8 * (4 + 2);
+    NSLog(@"y: %li", y);
     
     // do not alter
     return YES;  //
